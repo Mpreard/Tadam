16695ac4230ed73587a6cbb15c4429536f4f2491
Maxime <maxime.preard@ynov.com>
2020-09-11 23:32:03 +0200

    add all pictures

--- page2.php
+++ page2.php
@@ -3,20 +3,23 @@
   <head>
     <meta charset="utf-8" />
     <title>Galerie d'images</title>
+    <link rel="stylesheet" href="page2.css">
   </head>
   <body>
-    <div>
   <?php
     $dir = 'assets/img/*.jpg';
     $files = glob($dir,GLOB_BRACE);
 
     $nbrImg = count($files);
-
-    for($count = 1; $count <= $nbrImg; $count++)
-    {
-      echo '<div> <img src="./assets/img/' . $count . '.jpg"/></div>';
-    } 
   ?>
+      
+      <div id="grille">
+        <?php
+            for($count = 1; $count <= $nbrImg; $count++)
+            {
+          echo '<img class="image_grille" src="./assets/img/' . $count . '.jpg"/>';
+            }   
+        ?>
     </div>
   </body>
 </html>
