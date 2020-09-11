512e3ea193f7052fa9316496ec64b5e542d3da67
Maxime <maxime.preard@ynov.com>
2020-09-11 20:33:36 +0200

    add display pictures

--- page2.php
+++ page2.php
@@ -1,36 +1,22 @@
-<?php
-    $nom = $_GET['nom'];
-    $prenom = $_GET['prenom'];
-    echo 'Bienvenue sur la page de jeu '.$prenom;
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <title>Galerie d'images</title>
+  </head>
+  <body>
+    <div>
+  <?php
+    $dir = 'assets/img/*.jpg';
+    $files = glob($dir,GLOB_BRACE);
 
-    //Méthode 1 pour avoir l'adresse ip
-    echo '  adresse ip'.$_SERVER['REMOTE_ADDR'];
+    $nbrImg = count($files);
 
-    //Méthode 2 (car REMOTE_ADDR ne renvoi pas tout le temps l'adresse IP correct à cause d'un Proxy)
-    function getIp(){
-        if(!empty($_SERVER['HTTP_CLIENT_IP'])){
-          $ip = $_SERVER['HTTP_CLIENT_IP'];
-        }elseif(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
-          $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-        }else{
-          $ip = $_SERVER['REMOTE_ADDR'];
-        }
-        return $ip;
-      }
-
-
-
-
-
-
-
-
-
-
-
-
-    //Insertion ou Vérification de l'utilisateur dans la bdd
-    $requeteInsertion = "INSERT INTO user (id, first_name, last_name) VALUES ('', LOWER('".$prenom."'), LOWER('".$nom."'));";
-    $requeteCherche = "SELECT * FROM user WHERE first_name =LOWER('".$prenom."') AND last_name = LOWER('".$nom."');";
-
-?>
+    for($count = 1; $count <= $nbrImg; $count++)
+    {
+      echo '<div> <img src="./assets/img/' . $count . '.jpg"/></div>';
+    } 
+  ?>
+    </div>
+  </body>
+</html>
