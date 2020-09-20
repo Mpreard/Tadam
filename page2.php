bf9fc2e51224043c904e493fcb2a650abe98fde2
Maxime <maxime.preard@ynov.com>
2020-09-20 18:37:17 +0200

    cookie set up

--- page2.php
+++ page2.php
@@ -50,28 +50,28 @@
       </form>
     </div>
     <?php
-        function Submit_answer()
+      function Submit_answer()
+      {
+        $answer = htmlspecialchars($_GET['answer']);
+
+        if(isset($_GET["img"]))
         {
-          $answer = htmlspecialchars($_GET['answer']);
-
-          if(isset($_GET["img"]))
-          {
-            $bdd->exec('UPDATE image SET display = 1 WHERE url = '.$id_img.'');
-          } else 
-          {
-            echo "<script>alert(\"Veuillez choisir une image\")</script>";
-          }
-
-          if(isset($_COOKIE['ip']) && isset($_COOKIE['nom']) && isset($_COOKIE['prenom']))
-          {
-            $bdd->exec('INSERT INTO user(first_name, last_name, ip_user) VALUES("'.$_COOKIE['ip'].'","'.$_COOKIE['prenom'].'", "'.$_COOKIE['nom'].'", )');
-          }
-
-          $bdd->exec('INSERT INTO answer(user_answer) VALUES ("'.$answer.'")');
-
-          header("refresh:0, url=page2.php");
-
+          $bdd->exec('UPDATE image SET display = 1 WHERE url = '.$id_img.'');
+        } else 
+        {
+          echo "<script>alert(\"Veuillez choisir une image\")</script>";
         }
+
+        if(isset($_COOKIE['ip']) && isset($_COOKIE['nom']) && isset($_COOKIE['prenom']))
+        {
+          $bdd->exec('INSERT INTO user(first_name, last_name, ip_user) VALUES("'.$_COOKIE['ip'].'","'.$_COOKIE['prenom'].'", "'.$_COOKIE['nom'].'", )');
+        }
+
+        $bdd->exec('INSERT INTO answer(user_answer) VALUES ("'.$answer.'")');
+
+        header("refresh:0, url=page2.php");
+
+      }
     ?>
 
     
--- run.php
+++ run.php
@@ -1,8 +1,8 @@
 <?php
 
-setcookie('nom','caca', time() + 365*24*3600, null, null, false, true);
-setcookie('prenom','caca1', time() + 365*24*3600, null, null, false, true);
-setcookie('ip','ouais', time() + 365*24*3600, null, null, false, true);
+setcookie('nom',htmlspecialchars($_POST['nom']), time() + 365*24*3600, null, null, false, true);
+setcookie('prenom',htmlspecialchars($_POST['prenom']), time() + 365*24*3600, null, null, false, true);
+setcookie('ip',$_SERVER['REMOTE_ADDR'], time() + 365*24*3600, null, null, false, true);
 
 $code = '1234';
 
@@ -11,15 +11,10 @@ $nombre2 = $_POST['nombre2'];
 $nombre3 = $_POST['nombre3'];
 $nombre4 = $_POST['nombre4'];
 
-$nom = htmlspecialchars($_POST['nom']);
-$prenom = htmlspecialchars($_POST['prenom']);
-$ip = $_SERVER['REMOTE_ADDR'];
-
 $nombreFinal = $nombre1 . $nombre2 . $nombre3 . $nombre4;
 
-
 if($nombreFinal === $code){
-    header('Location: ./page2.php?nom='.$nom.'&prenom='.$prenom);
+    header('Location: ./page2.php');
 }else{
     header('Location: ./index.php?error=1');
     //header ("Refresh: 3;URL=index.html");
