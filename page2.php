4317ad9a03e3e3b2cf71907af8f3c01270bc5ad7
MaelFournier <mfournier2@la-joliverie.com>
2020-11-04 09:14:35 +0100

    Ajout css

--- page2.php
+++ page2.php
@@ -27,39 +27,41 @@
         </div>
       </div>
       <div class = "presentation_text">
-        <p class = "GG">Bien joué challenger, vous avez trouvé le code !</p>
+        <p class = "GG" id="first">Bien joué challenger, vous avez trouvé le code !</p>
         <p>Mais le jeu ne s'arrete pas la...</p>
         <p>Chaque jour, vous pourrez dévoiler une des cases et proposer une réponse.</p>
-        <p>Plus vous cumulez de réponses (correcte) plus vous aurez de chance d'être tiré au sort pour gagner  partie de jeu chez TADAM ESCAPE</p>
+        <p>Plus vous cumulez de réponses (correcte) plus vous aurez de chance d'être </br> tiré au sort pour gagner  partie de jeu chez TADAM ESCAPE</p>
         <p>Bon courage !</p>
-        <p>Alors ? Saurez-vous retrouver qui se cache derrière cette photo ?</p>
+        <p class="end_presentation">Alors ? Saurez-vous retrouver qui se cache derrière cette photo ?</p>
       </div>
-      <div id="grille">
-        <?php
-          $id = 1;
-          $clique = false;
-          $reponse = $bdd->query('SELECT url, display FROM image ORDER BY url ASC');
-          while ($donnees = $reponse->fetch())
-          {
-            if($donnees['display'] == 0)
+      <div class="wrap_grille">
+        <div id="grille">
+          <?php
+            $id = 1;
+            $clique = false;
+            $reponse = $bdd->query('SELECT url, display FROM image ORDER BY url ASC');
+            while ($donnees = $reponse->fetch())
             {
-              if(isset($_GET["img"]) && $id == $_GET["img"] && $clique == false)
+              if($donnees['display'] == 0)
               {
-                echo '<img class="image_grille" id='.$id.' src="assets/img-grille/' . $id . '.jpg"/>';
-                $clique = true;
-              } else {
-                echo'<a id='.$id.'  class="image_grille" href="page2.php?img='.$id.'"><img src="assets/img-grille/noir.png"/></a>';
+                if(isset($_GET["img"]) && $id == $_GET["img"] && $clique == false)
+                {
+                  echo '<img class="image_grille" id='.$id.' src="assets/img-grille/' . $id . '.jpg"/>';
+                  $clique = true;
+                } else {
+                  echo'<a id='.$id.'  class="image_grille" href="page2.php?img='.$id.'"><img src="assets/img-grille/noir.png"/></a>';
+                }
+              
               }
-            
+              elseif ($donnees['display'] == 1){
+                  echo '<img class="image_grille" id='.$id.' src="assets/img-grille/' . $id . '.jpg"/>';
+              } 
+              $id++;   
             }
-            elseif ($donnees['display'] == 1){
-                echo '<img class="image_grille" id='.$id.' src="assets/img-grille/' . $id . '.jpg"/>';
-            } 
-            $id++;   
-          }
-          $reponse->closeCursor();    
-        
-        ?>
+            $reponse->closeCursor();    
+          
+          ?>
+        </div>
       </div>
       <div>
         <form action="grille_back.php" method="POST">
