88b687c71664d0e8aa5deb7e3ba14448b45d6e77
MaelFournier <mfournier2@la-joliverie.com>
2020-11-25 16:07:48 +0100

    Ajout et fin(?) du responsive

--- error_page.php
+++ error_page.php
@@ -36,7 +36,7 @@
   <div class="bottom_page">
   </div>
   <?php 
-    header("refresh:3;url=index.php");
+    //header("refresh:3;url=index.php");
   ?>
 
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
