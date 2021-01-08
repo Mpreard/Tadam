346aee5a4c1557244125e825492bef6431a79dbd
MaelFournier <mfournier2@la-joliverie.com>
2021-01-08 21:30:40 +0100

    responsive end

--- index.php
+++ index.php
@@ -25,8 +25,8 @@
       <div class ="welcome message" id="welcome_message">
         <img class ="logo cadenas" src="assets/logos/logo_cadenas.png">
         <p class ="welcome_challenger"><strong> Bienvenue Challenger !</strong></p>
-        <p class ="enter_code">Veuillez entrer le code trouvé grâce à l'énigme.</p>
-        <p class = "GL"> Bonne chance ! </p>
+        <p class ="enter_code" id="enter_code">Veuillez entrer le code trouvé grâce à l'énigme.</p>
+        <p class = "GL" id="GL"> Bonne chance ! </p>
       </div>
     </div>
     <div class="form_container" id="form_container">
@@ -35,10 +35,10 @@
           <div class="row">
             <div class="col-12 jpa">
               <div class = code_container>
-                <input id="nombre1" name="nbr_1_code" class ="number_code" type="number" maxlength="1" autocomplete="false" required />
-                <input id="nombre2" name="nbr_2_code" class ="number_code" type="number" maxlength="1" autocomplete="false" required />
-                <input id="nombre3" name="nbr_3_code" class ="number_code" type="number" maxlength="1" autocomplete="false" required />
-                <input id="nombre4" name="nbr_4_code" class ="number_code number_left" type="number" maxlength="1" autocomplete="false" required />
+                <input id="nombre1" name="nbr_1_code" class ="number_code" type="number" maxlength="1" autocomplete="false"  onfocus="entry(2)" onblur="exit()" required />
+                <input id="nombre2" name="nbr_2_code" class ="number_code" type="number" maxlength="1" autocomplete="false" onfocus="entry(2)" onblur="exit()" required />
+                <input id="nombre3" name="nbr_3_code" class ="number_code" type="number" maxlength="1" autocomplete="false" onfocus="entry(2)" onblur="exit()" required />
+                <input id="nombre4" name="nbr_4_code" class ="number_code number_left" type="number" maxlength="1" autocomplete="false" onfocus="entry(2)" onblur="exit()" required />
               </div>
             </div>
           </div>
@@ -50,10 +50,10 @@
           <div class="row">
             <div class = "infos_container">
               <div class="col-6 input_infos">
-                <input type="email" id="email" name="email" class="form-control form_infos" placeholder="Email" autocomplete="false" onfocus="entry()" onblur="exit()" required />
+                <input type="email" id="email" name="email" class="form-control form_infos" placeholder="Email" autocomplete="false" onfocus="entry(1)" onblur="exit()" required />
               </div>
               <div class="col-6 input_infos">
-                <input type="text" id="prenom" name="prenom" class="form-control form_infos" placeholder="Prenom" autocomplete="false" onfocus="entry()" onblur="exit()" required />
+                <input type="text" id="prenom" name="prenom" class="form-control form_infos" placeholder="Prenom" autocomplete="false" onfocus="entry(1)" onblur="exit()" required />
               </div>
               <div class="col-6 input_infos input_submit_button">
                 <div class = "submit_button_class">
@@ -69,16 +69,32 @@
 
 <script>
 
-  function entry() {
+  function entry(n) {
     if (window.matchMedia("(max-width: 481px)").matches) {
+      if (n == 1) {
       $("html, body").animate({ scrollTop: $(document).height() }, 1000);
+      var text = document.getElementById('enter_code')
+      text.innerText ="Trouverez vous le bon code ?" ;
+      var text2 = document.getElementById('GL')
+      text2.style.display = "none";
+      }
+      if (n == 2) {
+      var text = document.getElementById('enter_code')
+      text.innerText ="Trouverez vous le bon code ?" ;
+      var text2 = document.getElementById('GL')
+      text2.style.display = "none";
+      }
+
     } else {
     }
   }
 
   function exit() {
     if (window.matchMedia("(max-width: 481px)").matches) {
-
+      var text = document.getElementById('enter_code')
+      text.innerText ="Veuillez entrer le code trouvé grâce à l'énigme." ;
+      var text2 = document.getElementById('GL')
+      text2.style.display = "block";
     } else {
     }
   }
